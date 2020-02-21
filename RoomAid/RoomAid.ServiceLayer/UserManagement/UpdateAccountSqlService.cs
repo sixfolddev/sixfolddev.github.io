f6ae53cc18c423b326ffb37791f9d5d70758b492
Djgione <thegiones13@sbcglobal.net>
2020-02-21 11:25:02 -0800

    fixed some naming conventions

--- RoomAid/RoomAid.ServiceLayer/UserManagement/UpdateAccountSqlService.cs
+++ RoomAid/RoomAid.ServiceLayer/UserManagement/UpdateAccountSqlService.cs
@@ -11,29 +11,40 @@ namespace RoomAid.ServiceLayer.UserManagement
 {
     public class UpdateAccountSqlService : IUpdateAccountService
     {
-        private  List<User> newUsers { get; }
-        private UpdateAccountDAO update { get; }
+        private readonly List<User> _newUsers;
+        private readonly UpdateAccountDAO _update;
 
+        /// <summary>
+        /// Service that crafts queries for updating users related to a sql database
+        /// </summary>
+        /// <param name="newUsers"></param>
+        /// <param name="update"></param>
         public UpdateAccountSqlService(List<User> newUsers, UpdateAccountDAO update)
         {
-            this.newUsers = newUsers;
-            this.update = update;
+            this._newUsers = newUsers;
+            this._update = update;
         }
-
+        /// <summary>
+        /// this is the publicly accessed interface method that returns an IResult based on success of updating all accounts
+        /// </summary>
+        /// <returns>CheckResult</returns>
         public IResult Update()
         {
             String message;
             bool isSuccess = true;
             List<String> queries = new List<String>();
             String tableName = ConfigurationManager.AppSettings["tableName"];
-            foreach(User newUser in newUsers)
+
+            //creates a set of queries within a list to pass towards a dao for updating
+            foreach(User newUser in _newUsers)
             {
                 queries.Add($"UPDATE {tableName} SET FirstName = {newUser.FirstName}, LastName = {newUser.LastName}," +
                     $"DateOfBirth = {newUser.DateOfBirth}, Gender = {newUser.Gender}, AccountStatus = {newUser.AccountStatus}" +
                     $" WHERE UserEmail = {newUser.UserEmail}");
             }
 
-            int rowsChanged = update.Update(queries);
+            //changing result based upon if all accounts were updated successfully
+            int rowsChanged = _update.Update(queries);
             if(rowsChanged == queries.Count)
             {
                 message = ConfigurationManager.AppSettings["successMessage"];
