d7d398f858943e8d1c3a8ed0db2204fb44a22fd1
Joshua-McDaniel <jake.mcd.cs@gmail.com>
2020-02-27 13:28:15 -0800

    Delete Rework

---
+++ RoomAid/RoomAid.DataAccessLayer/UserManagement/SqlDeleteAccountDAO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAid.DataAccessLayer
+{
+    public class SqlDeleteAccountDAO : IDeleteAccountDAO
+    {
+        private readonly String _connection;
+        public SqlDeleteAccountDAO(String connection)
+        {
+            this._connection = connection;
+        }
+        /// <summary>
+        /// Execution of the list of queries provided by the service
+        /// </summary>
+        /// <param name="commands"></param>
+        /// <returns></returns>
+        public int Delete(List<SqlCommand> commands)
+        {
+            int rowsDeleted = 0;
+            using (SqlConnection connection = new SqlConnection(_connection))
+            {
+                connection.Open();
+                SqlTransaction trans = connection.BeginTransaction();
+                try
+                {
+                    foreach(SqlCommand cmd in commands)
+                    {
+                        cmd.Connection = connection;
+                        cmd.Transaction = trans;
+                        rowsDeleted += cmd.ExecuteNonQuery();
+                    }
+                    trans.Commit();
+                }
+                catch(Exception)
+                {
+                    trans.Rollback();
+                    throw;
+                }
+            }
+            return rowsDeleted;
+        }
+    }
+}
---
+++ RoomAid/RoomAid.ServiceLayer/UserManagement/DeleteAccountSQLService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Configuration;
+using System.Data.SqlClient;
+using RoomAid.DataAccessLayer;
+
+namespace RoomAid.ServiceLayer.UserManagement
+{
+    public class DeleteAccountSQLService : IDeleteAccountService
+    {
+        private readonly List<User> _targetUsers;
+        private readonly IDeleteAccountDAO _delete;
+        /// <summary>
+        /// Craft queries based off a single user
+        /// </summary>
+        /// <returns></returns>
+        public DeleteAccountSQLService(User targetUser, IDeleteAccountDAO delete)
+        {
+            this._targetUsers = new List<User>();
+            this._targetUsers.Add(targetUser);
+            this._delete = delete;
+        }
+        /// <summary>
+        /// Craft queries based off of multiple users
+        /// </summary>
+        /// <returns></returns>
+        public DeleteAccountSQLService(List<User> targetUsers, IDeleteAccountDAO delete)
+        {
+            this._targetUsers = targetUsers;
+            this._delete = delete;
+        }
+        public IResult Delete()
+        {
+            string message = "";
+            bool isSuccess = true;
+            List<SqlCommand> commands = new List<SqlCommand>();
+            foreach(User targetUser in _targetUsers)
+            {
+                var cmd = new SqlCommand(ConfigurationManager.AppSettings["deleteCmd"]);
+                cmd.Parameters.AddWithValue("@email", targetUser.UserEmail);
+                commands.Add(cmd);
+            }
+            int rowsDeleted = _delete.Delete(commands);
+            if(rowsDeleted == commands.Count)
+            {
+                message += ConfigurationManager.AppSettings["DeleteAccountSuccess"];
+            }
+            else
+            {
+                message += ConfigurationManager.AppSettings["DeleteAccountFailure"];
+                isSuccess = false;
+            }
+            return new CheckResult(message, isSuccess);
+        }
+    }
+}
