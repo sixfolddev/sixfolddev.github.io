3598c8253c26de4f9c5b4316b7deb6a1d723f166
Joshua-McDaniel <jake.mcd.cs@gmail.com>
2020-03-09 15:46:19 -0700

    Test

--- RoomAid/RoomAid.UserManagement.Tests/DeleteAccountSqlServiceTest.cs
+++ RoomAid/RoomAid.UserManagement.Tests/DeleteAccountSqlServiceTest.cs
@@ -6,58 +6,51 @@ using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoomAid.DataAccessLayer;
 using RoomAid.ServiceLayer;
-using RoomAid.ServiceLayer.UserManagement;
 
 namespace UserManagementTests
 {
     [TestClass]
     public class DeleteAccountSqlServiceTest
     {
+        private ICreateAccountDAO newAccountDAO = new SqlCreateAccountDAO(ConfigurationManager.AppSettings["sqlConnectionAccount"]);
+        private ICreateAccountDAO newMappingDAO = new SqlCreateAccountDAO(ConfigurationManager.AppSettings["sqlConnectionMapping"]);
+        private IMapperDAO mapperDAO = new SqlMapperDAO(ConfigurationManager.AppSettings["sqlConnectionMapping"]);
+        private ICreateAccountDAO newUserDAO = new SqlCreateAccountDAO(ConfigurationManager.AppSettings["sqlConnectionSystem"]);
 
         [TestMethod]
-        public void DeleteUsers_Pass()
+        //Create a new acccount and a matching user. Execute deletion for a single user.
+        public void DeleteSingleUser_Pass()
         {
-            var newUsers = new List<User>();
-            var date = new DateTime(1998, 11, 13);
-            var user = new User(1, "boi@gmail.com", "daniel", "gione", "enabled", date, "Male");
-            newUsers.Add(user);
-            var dao = new UpdateAccountDAOTestSuccess(date);
-            var update = new UpdateAccountSqlService(newUsers, dao);
+            //Arrange
+            bool expected = true;
 
+            Account testAccount = new Account("testerEmail", "testHashedPassword", "testSalt");
+            CreateAccountDAOs daos = new CreateAccountDAOs(newAccountDAO, newMappingDAO, newUserDAO, mapperDAO);
+            ICreateAccountService cas = new SqlCreateAccountService(testAccount, daos);
+            cas.Create();
+            User testUser = new User(mapperDAO.GetSysID(testAccount.UserEmail), "testerEmail", "Collin", "Damarines", "Activated", DateTime.Now, "Male");
+           
+            //Act
             IDeleteAccountDAO systemDB = new SqlDeleteAccountDAO(ConfigurationManager.AppSettings["sqlConnectionSystem"]);
             IDeleteAccountDAO mappingDB = new SqlDeleteAccountDAO(ConfigurationManager.AppSettings["sqlConnectionMapping"]);
             IDeleteAccountDAO accountDB = new SqlDeleteAccountDAO(ConfigurationManager.AppSettings["sqlConnectionAccount"]);
-
-            IDeleteAccountService deleter = new DeleteAccountSQLService(user, systemDB, mappingDB, accountDB);
+            IDeleteAccountService deleter = new DeleteAccountSQLService(testUser, systemDB, mappingDB, accountDB);
+            IResult deleteResult = deleter.Delete();
+            bool actual = deleteResult.IsSuccess;
+            Console.WriteLine(deleteResult.Message);
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
-        public class UpdateAccountDAOTestSuccess : IUpdateAccountDAO
+        [TestMethod]
+        //Create multiple accounts and users. Execute deletion of multiple users.
+        public void DeleteMultipleUsers_Pass()
         {
-            public DateTime date;
-            public UpdateAccountDAOTestSuccess(DateTime date)
-            {
-                this.date = date;
-            }
+            //Arrange
+            bool expected = true;
 
-            public int Update(List<SqlCommand> commands)
-            {
-
-                var cmd = new SqlCommand();
-                cmd.CommandText = "UPDATE dbo.Users SET FirstName = @fName, LastName = @lName, DateOfBirth = @dob, Gender = @gender, AccountStatus = @status WHERE UserEmail = @email";
-                cmd.Parameters.AddWithValue("@email", "boi@gmail.com");
-                cmd.Parameters.AddWithValue("@fName", "daniel");
-                cmd.Parameters.AddWithValue("@lName", "gione");
-                cmd.Parameters.AddWithValue("@dob", date);
-                cmd.Parameters.AddWithValue("@gender", "Male");
-                cmd.Parameters.AddWithValue("@status", "enabled");
-
-                Trace.WriteLine(cmd.CommandText);
-                Trace.WriteLine(commands[0].CommandText);
-                if (commands[0].CommandText.Equals(cmd.CommandText))
-                    return 1;
-                else
-                    return 0;
-            }
+            Account test c
         }
     }
 }
