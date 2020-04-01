684c318031d0333a808322b08c89360c96c7ef70
Djgione <thegiones13@sbcglobal.net>
2020-03-31 19:57:15 -0700

    updateAccountSqlServiceTest using proper sql connections

--- RoomAid/RoomAid.UserManagement.Tests/UpdateAccountSqlServiceTest.cs
+++ RoomAid/RoomAid.UserManagement.Tests/UpdateAccountSqlServiceTest.cs
@@ -39,8 +39,9 @@ namespace UserManagementTests
             var result = new CheckResult("All targeted rows have been updated successfully!", true);
             var date = new DateTime(1998, 11, 13);
             var user = new User(1, "boi@gmail.com", "daniel", "gione", "disabled", date, "Male");
-            var dao = new UpdateAccountDAOTestSuccess(date);
-            var update = new UpdateAccountSqlService(user, dao);
+            var dao = new UpdateAccountSqlDAO("fakeConnect");
+
+            var update = new UpdateAccountSqlService(user,dao );
 
             var compare = update.Update();
 
@@ -48,34 +49,34 @@ namespace UserManagementTests
             Assert.AreNotEqual(compare.Message, result.Message);
         }
 
-        public class UpdateAccountDAOTestSuccess: IUpdateAccountDAO
-        {
-            public DateTime date;
-            public UpdateAccountDAOTestSuccess(DateTime date)
-            {
-                this.date = date;
-            }
+        //public class UpdateAccountDAOTestSuccess: IUpdateAccountDAO
+        //{
+        //    public DateTime date;
+        //    public UpdateAccountDAOTestSuccess(DateTime date)
+        //    {
+        //        this.date = date;
+        //    }
 
-            public int Update(List<SqlCommand> commands)
-            {
+        //    public int Update(List<SqlCommand> commands)
+        //    {
 
-                var cmd = new SqlCommand();
-                cmd.CommandText= "UPDATE dbo.Users SET FirstName = @fName, LastName = @lName, DateOfBirth = @dob, Gender = @gender, AccountStatus = @status WHERE UserEmail = @email";
-                cmd.Parameters.AddWithValue("@email", "boi@gmail.com");
-                cmd.Parameters.AddWithValue("@fName", "daniel");
-                cmd.Parameters.AddWithValue("@lName", "gione");
-                cmd.Parameters.AddWithValue("@dob", date);
-                cmd.Parameters.AddWithValue("@gender", "Male");
-                cmd.Parameters.AddWithValue("@status", "enabled");
+        //        var cmd = new SqlCommand();
+        //        cmd.CommandText= "UPDATE dbo.Users SET FirstName = @fName, LastName = @lName, DateOfBirth = @dob, Gender = @gender, AccountStatus = @status WHERE UserEmail = @email";
+        //        cmd.Parameters.AddWithValue("@email", "boi@gmail.com");
+        //        cmd.Parameters.AddWithValue("@fName", "daniel");
+        //        cmd.Parameters.AddWithValue("@lName", "gione");
+        //        cmd.Parameters.AddWithValue("@dob", date);
+        //        cmd.Parameters.AddWithValue("@gender", "Male");
+        //        cmd.Parameters.AddWithValue("@status", "enabled");
                 
-                Trace.WriteLine(cmd.CommandText);
-                Trace.WriteLine(commands[0].CommandText);
-                if (commands[0].CommandText.Equals(cmd.CommandText))
-                    return 1;
-                else
-                    return 0;
-            }
-        }
+        //        Trace.WriteLine(cmd.CommandText);
+        //        Trace.WriteLine(commands[0].CommandText);
+        //        if (commands[0].CommandText.Equals(cmd.CommandText))
+        //            return 1;
+        //        else
+        //            return 0;
+        //    }
+        //}
 
         
 
