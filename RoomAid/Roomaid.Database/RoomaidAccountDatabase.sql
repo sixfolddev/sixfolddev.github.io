a03bc196663c0bbe3c37abf0538c5d4f3a4d9f73
MichellKuang <michellkuang96@yahoo.com>
2020-04-03 01:33:46 -0700

    fixed problems with insufficient storage for hash pw and salt in database

--- RoomAid/Roomaid.Database/RoomaidAccountDatabase.sql
+++ RoomAid/Roomaid.Database/RoomaidAccountDatabase.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Accounts(
 	UserEmail VARCHAR(200) NOT NULL,
-	HashPassword VARCHAR(32) NOT NULL,
-	Salt VARCHAR(32) NOT NULL,
+	HashPassword VARCHAR(64) NOT NULL,
+	Salt VARCHAR(64) NOT NULL,
 	CONSTRAINT pk_Accounts PRIMARY KEY (UserEmail)
 );
