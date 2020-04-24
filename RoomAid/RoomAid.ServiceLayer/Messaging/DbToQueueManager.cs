9701b1fc9e5764b3ec07e60963d9734682c98cbd
MichellKuang <michellkuang96@yahoo.com>
2020-04-23 18:53:00 -0700

    Setting up MSMQ

---
+++ RoomAid/RoomAid.ServiceLayer/Messaging/DbToQueueManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Messaging;
+using System.Text;
+
+namespace RoomAid.ServiceLayer.Messaging
+{
+    public class DbToQueueManager
+    {
+        public DbToQueueManager()
+        {
+
+        }
+
+        [SqlProcedure]
+        public static void Send(SqlString queueName, SqlString message)
+        {
+            var queue = queueName.Value;
+            if (queueName == null || queue == null)
+            {
+                throw new Exception("The message queue name cannot be null.");
+            }
+            CreateQueue(queue); // Creates the queue on the system
+            try
+            {
+                using (var messageQueue = new MessageQueue(queue, QueueAccessMode.Send))
+                {
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static void CreateQueue(string queuePath)
+        {
+            try
+            {
+                if (!MessageQueue.Exists(queuePath))
+                {
+                    MessageQueue.Create(queuePath);
+                }
+                else
+                {
+                    var stringBuilder = new StringBuilder(queuePath);
+                    stringBuilder.Append(" already exists. Using existing queue.");
+                    Trace.Write(stringBuilder.ToString());
+                }
+            }
+            catch (MessageQueueException e)
+            {
+                throw e;
+            }
+        }
+        
+    }
+}
---
+++ RoomAid/RoomAid.ServiceLayer/Messaging/IMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoomAid.ServiceLayer.Messaging
+{
+    interface IMessage
+    {
+        // ID of the current message
+        string MessageID { get; set; }
+        // ID of the message that was replied to (if any)
+        string PrevMessageID { get; set; }
+        // System ID of the user sending the message
+        string SenderID { get; set; }
+        // Timestamp of when a message is sent
+        DateTime SentDate { get; set; }
+        // Indicates whether a message is read or unread
+        bool IsRead { get; set; }
+        
+    }
+}
