598ba68b8eef23511164daa0a654e9370fb3b503
Jace <jacenmark131@yahoo.com>
2020-05-04 23:09:24 -0700

    Implemented HHListingDAO.Insert()
    Insert()

--- RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
+++ RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
-namespace RoomAid.DataAccessLayer.HouseHoldListing
+namespace RoomAid.DataAccessLayer.HouseHoldListing 
 {
     public class HHListingDAO
     {
@@ -17,7 +18,39 @@ namespace RoomAid.DataAccessLayer.HouseHoldListing
 
         public int Insert(HHListingModel model)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_dbConnectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction("HouseholdListing Creation");
+
+                try
+                {
+                    var query = "INSERT into dbo.HouseholdListings(HID, HouseholdType, ListingDescription, Price, DatePosted) VALUES (@hid, @type, @desc, @price, @date)";
+                    using(SqlCommand command = new SqlCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@hid", model.HID);
+                        command.Parameters.AddWithValue("@date", model.DatePosted);
+                        command.Parameters.AddWithValue("@type", model.HouseholdType);
+                        command.Parameters.AddWithValue("@desc", model.ListingDescription);
+                        command.Parameters.AddWithValue("@price", model.Price);
+
+                        var result = command.BeginExecuteNonQuery();
+
+                        var rowsChanged = command.EndExecuteNonQuery(result);
+                        if (rowsChanged != 1)
+                        {
+                            throw new Exception("Error in Household Listing Creation");
+                        }
+                        transaction.Commit();
+                        return rowsChanged;
+                    }
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    throw e;
+                }
+            }
         }
 
         public int Update(HHListingModel model)
