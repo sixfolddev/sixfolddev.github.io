74922d62c435a328b6f92e345a0d013c98ceca77
Jace <jacenmark131@yahoo.com>
2020-05-05 16:58:48 -0700

    Update HouseHoldListingService.cs
    Initial Impelmentations of functions...
    Future work: Where to log, should I be throwing exceptions or just returning false?
    
    Do I just log failure outside the for loop and log success in the inner try catch loops?

--- RoomAid/RoomAid.ServiceLayer/HouseHoldListing/HouseHoldListingService.cs
+++ RoomAid/RoomAid.ServiceLayer/HouseHoldListing/HouseHoldListingService.cs
@@ -10,28 +10,92 @@ namespace RoomAid.ServiceLayer.HouseHoldListing
 {
     public class HouseHoldListingService
     {
-        private readonly IHHListingDAO hhListingDAO;
+        private readonly IHHListingDAO dao;
+        private const int numberOfRetries = 3;
 
         public HouseHoldListingService(IHHListingDAO dao)
         {
-            this.hhListingDAO = dao;
+            this.dao = dao;
         }
 
-        public int Insert(int hid)
+        public bool Insert(int hid)
         {
-            throw new NotImplementedException();
+            var model = new HHListingModel(hid, DateTime.UtcNow, SqlBoolean.False);
+            for (int i = 0; i < numberOfRetries; i++)
+            {
+                try {
+                    var rowsChanged = dao.Insert(model);
+                    if (rowsChanged == 1)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Should Logging be done for every failed attempt?
+                }
+            }
+            throw new Exception("Error encountered in HouseholdListing Insert");
+
         }
-        public int Update(int hid, string zipCode, string address, SqlBoolean availability, string hhType, decimal price, string description)
+
+        public bool Update(int hid, string zipCode, string address, SqlBoolean availability, string hhType, decimal price, string description)
         {
-            throw new NotImplementedException();
+            var model = new HHListingModel(hid, DateTime.UtcNow, availability, zipCode: zipCode, streetAddress: address, householdType: hhType, listingDescription: description, price: price);
+            for (int i = 0; i < numberOfRetries; i++)
+            {
+                try
+                {
+                    var rowsChanged = dao.Update(model);
+                    if (rowsChanged == 1)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Should Logging be done for every failed attempt?
+                }
+            }
+
+            throw new Exception("Error encountered in HouseholdListing Update");
         }
+
         public HHListingModel Retrieve(int hid)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < numberOfRetries; i++)
+            {
+                try
+                {
+                    var model = dao.Retrieve(hid);
+                    return model;
+                }
+                catch (Exception e)
+                {
+                    //Should Logging be done for every failed attempt?
+                }
+            }
+            throw new Exception("Error encountered in HouseholdListing Retrieve");
+           
         }
-        public int Delete(int hid)
+        public bool Delete(int hid)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < numberOfRetries; i++)
+            {
+                try
+                {
+                    var rowsChanged = dao.Delete(hid);
+                    if (rowsChanged == 1)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Should Logging be done for every failed attempt?
+                }
+            }
+            throw new Exception("Error encountered in HouseholdListing Delete");
         }
         
     }
