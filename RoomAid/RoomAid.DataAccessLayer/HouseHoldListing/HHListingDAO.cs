7357f71dfa71e157a36852a2148000e324e10360
Jace <jacenmark131@yahoo.com>
2020-05-04 22:50:35 -0700

    Updated HHListingDAO
    Initial commit for HHLIstingDAO. Beginning coding of Insert.

--- RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
+++ RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace RoomAid.DataAccessLayer.HouseHoldListing
 {
-    class HHListingDAO
+    public class HHListingDAO
     {
+        private readonly string _dbConnectionString;
+
+        HHListingDAO(string connection)
+        {
+            this._dbConnectionString = connection;
+        }
+
+        public int Insert(HHListingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(HHListingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int hid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Retrieve(int hid)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
