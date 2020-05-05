d052cddd9c482b91c4fb97cdf661965fde4c7f72
Jace <jacenmark131@yahoo.com>
2020-05-05 16:31:14 -0700

    Update HouseHoldListingService.cs
    Start of implementation

--- RoomAid/RoomAid.ServiceLayer/HouseHoldListing/HouseHoldListingService.cs
+++ RoomAid/RoomAid.ServiceLayer/HouseHoldListing/HouseHoldListingService.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RoomAid.DataAccessLayer.HouseHoldListing;
 
 namespace RoomAid.ServiceLayer.HouseHoldListing
 {
-    class HouseHoldListingService
+    public class HouseHoldListingService
     {
+        private readonly IHHListingDAO hhListingDAO;
+
+        public HouseHoldListingService(IHHListingDAO dao)
+        {
+            this.hhListingDAO = dao;
+        }
+
+        public int Insert(int hid)
+        {
+            throw new NotImplementedException();
+        }
+        public int Update(int hid, string zipCode, string address, SqlBoolean availability, string hhType, decimal price, string description)
+        {
+            throw new NotImplementedException();
+        }
+        public HHListingModel Retrieve(int hid)
+        {
+            throw new NotImplementedException();
+        }
+        public int Delete(int hid)
+        {
+            throw new NotImplementedException();
+        }
+        
     }
 }
