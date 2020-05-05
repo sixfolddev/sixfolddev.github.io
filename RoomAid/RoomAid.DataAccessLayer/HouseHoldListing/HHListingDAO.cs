7ee1fdb16bf184c2bc873e4e4c879cbe78954369
Jace <jacenmark131@yahoo.com>
2020-05-05 16:05:04 -0700

    Update HouseholdListing.DAO
    Initial implementations of Retrieve and Delete.
    Edited error string for exceptions for Insert, Update, Retreive, and Delete.

--- RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
+++ RoomAid/RoomAid.DataAccessLayer/HouseHoldListing/HHListingDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace RoomAid.DataAccessLayer.HouseHoldListing 
 {
@@ -39,7 +40,7 @@ namespace RoomAid.DataAccessLayer.HouseHoldListing
                         var rowsChanged = command.EndExecuteNonQuery(result);
                         if (rowsChanged != 1)
                         {
-                            throw new Exception("Error in Household Listing Creation");
+                            throw new Exception("Error encountered in Household Listing Creation");
                         }
                         transaction.Commit();
                         return rowsChanged;
@@ -91,7 +92,7 @@ namespace RoomAid.DataAccessLayer.HouseHoldListing
 
                         if (rowsChanged != 1)
                         {
-                            throw new Exception("Error in Household Listing Update");
+                            throw new Exception("Error encountered in Household Listing Update");
                         }
 
                         transaction.Commit();
@@ -106,14 +107,127 @@ namespace RoomAid.DataAccessLayer.HouseHoldListing
             }
         }
 
+        /// <summary>
+        /// Deletes a single household listing record from the database that belongs to a specific db.
+        /// </summary>
+        /// <param name="hid"></param>
+        /// <returns></returns>
         public int Delete(int hid)
         {
-            throw new NotImplementedException();
+            using(var connection = new SqlConnection(_dbConnectionString))
+            {
+                var transaction = connection.BeginTransaction("HouseholdListing Delete");
+
+                try
+                {
+                    var query = "Delete from dbo.HouseholdListings WHERE HID = @hid";
+
+                    using (var command = new SqlCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@hid", hid);
+
+                        var result = command.BeginExecuteNonQuery();
+
+                        var rowsChanged = command.EndExecuteNonQuery(result);
+
+                        if (rowsChanged != 1)
+                        {
+                            throw new Exception("Error encountered in Household Deletion");
+                        }
+
+                        transaction.Commit();
+                        return rowsChanged;
+                    }
+                }
+                catch(Exception e)
+                {
+                    transaction.Rollback();
+                    throw e;
+                }
+            }
         }
 
-        public int Retrieve(int hid)
+        public HHListingModel Retrieve(int hid)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_dbConnectionString))
+            {
+                var transaction = connection.BeginTransaction("HouseholdListing Retrieve");
+
+                try
+                {
+                    var query = "SELECT ZipCode, Availability from dbo.Households WHERE HID = @hid;" +
+                                "SELECT ListingDescription, Price, DatePosted from dbo.HouseholdListings WHERE HID = @hid;" +
+                                "SELECT FirstName, LastName from Residents INNER JOIN Users on Residents.SysID = Users.SysID WHERE HID = @hid;";
+
+                    using (var command = new SqlCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@hid", hid);
+
+                        var result= command.BeginExecuteReader();
+
+                        var reader = command.EndExecuteReader(result);
+
+                        #region Reads 1st Result of the batch query specified in var query.
+
+                        if (!reader.Read())
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        } 
+                        var zip = (string)reader["ZipCode"];
+                        var availability = (SqlBoolean)reader["Availability"];//If there are still rows in the query, then the program has retrieved more than 1 householdListing with specified HID
+                        if (reader.Read())
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        }
+
+                        #endregion
+
+
+                        #region Reads 2nd result of the batch query specified in var query.
+                        reader.NextResult(); 
+                        if (!reader.Read())//Read the first result of the batch query specified in var query.
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        }
+                        var datePosted = (DateTime)reader["DatePosted"];
+                        var description = (string)reader["ListingDescription"];
+                        var price = (decimal)reader["Price"];
+                        //If there are still rows in the query, then the program has retrieved more than 1 householdListing with specified HID
+                        if (reader.Read())
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        }
+                        #endregion
+
+
+                        #region Reads the third result set of the batch query specified in var query.
+                        reader.NextResult();
+                        if (!reader.Read())//Read the first result of the batch query specified in var query.
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        }
+                        var firstName = (string)reader["FirstName"];
+                        var lastName = (string)reader["LastName"];
+                        var hostName = firstName + lastName;
+                        //If there are still rows in the query, then the program has retrieved more than 1 householdListing with specified HID
+                        if (reader.Read())
+                        {
+                            throw new Exception("Error encountered in HouseholdListing Retrieval");
+                        }
+                        #endregion
+
+                        var model = new HHListingModel(hid, datePosted, availability, hostName: hostName, zipCode: zip, listingDescription: description, price: price);
+                        transaction.Commit();
+                        return model;
+                        
+                    }
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    throw e;
+                }
+            }
         }
     }
 }
