59c0e07f4159215134c8cdc71513da8cca8dc59f
Jace <jacenmark131@yahoo.com>
2020-05-05 18:37:21 -0700

    Revert "Revert "Initial Unit Tests""
    This reverts commit 1f5e7abf2d6adf5be6e49d6ac1910c8c344d0fe7.

---
+++ RoomAid/RoomAid.HouseholdListing.Tests/HHListingDAOTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomAid.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RoomAid.HouseholdListing.Tests
+{
+    [TestClass]
+    public class HHListingDAOTests
+    {
+        private RoomAid.DataAccessLayer.HouseHoldListing.HHListingDAO dao;
+        private readonly string envConnectionString = "sqlConnectionSystemWithDatePosted";
+        private readonly int testHID = 012393;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            dao = new DataAccessLayer.HouseHoldListing.HHListingDAO(Environment.GetEnvironmentVariable(envConnectionString, EnvironmentVariableTarget.User));
+            var model = new RoomAid.DataAccessLayer.HouseHoldListing.HHListingModel(testHID);
+        }
+
+        [TestMethod]
+        public void InsertTest_Pass()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+        }
+    }
+}
